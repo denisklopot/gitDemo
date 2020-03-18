31d5102499503fdfc69d44f7a1748f09cd980b16
Denis Klopat <denisklopot0@gmail.com>
2020-03-18 16:40:48 +0300

    initial commit

---
+++ gitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitDemo
+//
+//  Created by Денис  Клопот on 3/18/20.
+//  Copyright © 2020 Denis Klopot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
